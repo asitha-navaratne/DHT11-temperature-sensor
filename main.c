bbfc33da0b4c401c2cececb477e1596a8630c930
Asitha Navaratne <asithan476@gmail.com>
2021-10-02 21:24:31 +0530

    Justified comments

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 #include <stdlib.h>
 #include <util/delay.h>
 
-#define DHT11_DDR		DDRC									///< DDR of DHT11 sensor.
-#define LCD_DDR 		DDRB									///< DDR of LCD display.
-#define	DHT11_INPUT		PINC									///< Port of DHT11 sensor for input operations.
-#define	DHT11_OUTPUT	PORTC									///< Port of DHT11 sensor for output operations.
-#define LCD_PORT 		PORTB									///< Port of LCD display.
-#define DHT11_PIN		PC0										///< Pin for DHT11 sensor.
-#define RS 				PB0										///< Pin for LCD Register Select terminal.
-#define EN 				PB1										///< Pin for LCD Enable terminal.
+#define DHT11_DDR		DDRC						///< DDR of DHT11 sensor.
+#define LCD_DDR 		DDRB						///< DDR of LCD display.
+#define	DHT11_INPUT		PINC						///< Port of DHT11 sensor for input operations.
+#define	DHT11_OUTPUT		PORTC						///< Port of DHT11 sensor for output operations.
+#define LCD_PORT 		PORTB						///< Port of LCD display.
+#define DHT11_PIN		PC0						///< Pin for DHT11 sensor.
+#define RS 			PB0						///< Pin for LCD Register Select terminal.
+#define EN 			PB1						///< Pin for LCD Enable terminal.
 
 void DHT11_START_PULSE(void);
 unsigned char DHT11_DATA_BYTE(void);
@@ -30,14 +30,14 @@ int main(void){
 	while(1){
 		DHT11_START_PULSE();
 		
-		humidity_int = DHT11_DATA_BYTE();					///< Receive integer value of humidity.
+		humidity_int = DHT11_DATA_BYTE();				///< Receive integer value of humidity.
 		humidity_decimal = DHT11_DATA_BYTE();				///< Receive decimal value of humidity.
 		temperature_int = DHT11_DATA_BYTE();				///< Receive integer value of temperature.
 		temperature_decimal = DHT11_DATA_BYTE();			///< Receive decimal value of temperature.
-		checksum = DHT11_DATA_BYTE();						///< Receive checksum  value.
+		checksum = DHT11_DATA_BYTE();					///< Receive checksum  value.
 		
-		LCD_SEND_COMMAND(0x01);								///< Clear display screen.
-		LCD_SEND_COMMAND(0x80);								///< Move cursor to beginning of first line.
+		LCD_SEND_COMMAND(0x01);						///< Clear display screen.
+		LCD_SEND_COMMAND(0x80);						///< Move cursor to beginning of first line.
 		
 		if(checksum != (uint8_t)(humidity_int + humidity_decimal + temperature_int + temperature_decimal)){
 			LCD_SEND_STRING("Error!");
@@ -48,7 +48,7 @@ int main(void){
 			LCD_SEND_CHARACTER('.');
 			
 			LCD_SEND_NUMBER(temperature_decimal,1);
-			LCD_SEND_CHARACTER(0xDF);						///< Send a degree symbol to the LCD display.
+			LCD_SEND_CHARACTER(0xDF);				///< Send a degree symbol to the LCD display.
 			LCD_SEND_CHARACTER('C');
 			
 			LCD_SEND_COMMAND(0xC0);
@@ -72,11 +72,11 @@ int main(void){
 void LCD_DISPLAY_INIT(void){
 	LCD_DDR |= (1<<RS)|(1<<EN)|(1<<4)|(1<<5)|(1<<6)|(1<<7);
 	
-	LCD_SEND_COMMAND(0x01);										///< Clear display screen.
-	LCD_SEND_COMMAND(0x02);										///< Initialize LCD in 4-bit mode.
-	LCD_SEND_COMMAND(0x28);										///< 2 line, 5x7 matrix of characters in 4-bit mode.
-	LCD_SEND_COMMAND(0x80);										///< Move cursor to beginning of first line.
-	LCD_SEND_COMMAND(0x0C);										///< Turn display and cursor on.
+	LCD_SEND_COMMAND(0x01);							///< Clear display screen.
+	LCD_SEND_COMMAND(0x02);							///< Initialize LCD in 4-bit mode.
+	LCD_SEND_COMMAND(0x28);							///< 2 line, 5x7 matrix of characters in 4-bit mode.
+	LCD_SEND_COMMAND(0x80);							///< Move cursor to beginning of first line.
+	LCD_SEND_COMMAND(0x0C);							///< Turn display and cursor on.
 	_delay_ms(2);
 }
 
@@ -88,13 +88,13 @@ void DHT11_START_PULSE(void){
 	DHT11_DDR |= (1<<DHT11_PIN);
 	DHT11_OUTPUT &= ~(1<<DHT11_PIN);
 	_delay_ms(20);
-	DHT11_OUTPUT |= (1<<DHT11_PIN);				///< Send a 20 ms LOW pulse to the sensor.
+	DHT11_OUTPUT |= (1<<DHT11_PIN);						///< Send a 20 ms LOW pulse to the sensor.
 	
 	DHT11_DDR &= ~(1<<DHT11_PIN);
 	
 	while(DHT11_INPUT & (1<<DHT11_PIN));
 	while(!(DHT11_INPUT & (1<<DHT11_PIN)));
-	while(DHT11_INPUT & (1<<DHT11_PIN));			///< Wait until the response pulse is received.
+	while(DHT11_INPUT & (1<<DHT11_PIN));					///< Wait until the response pulse is received.
 }
 
 /*!
@@ -108,10 +108,10 @@ unsigned char DHT11_DATA_BYTE(void){
 		while(!(DHT11_INPUT & (1<<DHT11_PIN)));
 		_delay_us(30);
 		if(DHT11_INPUT & (1<<DHT11_PIN)){
-			databyte = (databyte<<1)|(0x01);					///< If data line is HIGH for more than 30 us, received bit is a 1.
+			databyte = (databyte<<1)|(0x01);			///< If data line is HIGH for more than 30 us, received bit is a 1.
 		}
 		else{
-			databyte = (databyte<<1);							///< If data line is LOW after 30 us, received bit is a 0.
+			databyte = (databyte<<1);				///< If data line is LOW after 30 us, received bit is a 0.
 		}
 		while(DHT11_INPUT & (1<<DHT11_PIN));
 	}
@@ -125,17 +125,17 @@ unsigned char DHT11_DATA_BYTE(void){
 
 void LCD_SEND_COMMAND(unsigned char command){
 	LCD_PORT = (LCD_PORT & 0x0F)|(command & 0xF0);				///< Load upper nibble of the command to the port.
-	LCD_PORT &= ~(1<<RS);										///< Set RS pin low to enter command mode.
+	LCD_PORT &= ~(1<<RS);							///< Set RS pin low to enter command mode.
 	LCD_PORT |= (1<<EN);
 	_delay_us(1);
-	LCD_PORT &= ~(1<<EN);										///< Create a momentary pulse at EN pin to send the data to the LCD.
+	LCD_PORT &= ~(1<<EN);							///< Create a momentary pulse at EN pin to send the data to the LCD.
  
 	_delay_us(200);
  
 	LCD_PORT = (LCD_PORT & 0x0F)|(command << 4);				///< Load lower nibble of the command to the port.
 	LCD_PORT |= (1<<EN);
 	_delay_us(1);
-	LCD_PORT &= ~(1<<EN);										///< Pulse to send the data.
+	LCD_PORT &= ~(1<<EN);							///< Pulse to send the data.
  
 	_delay_ms(2);
 }
@@ -147,17 +147,17 @@ void LCD_SEND_COMMAND(unsigned char command){
 
 void LCD_SEND_CHARACTER(unsigned char character){
 	LCD_PORT = (LCD_PORT & 0x0F)|(character & 0xF0);			///< Load upper nibble of the character to the port. 
-	LCD_PORT |= (1<<RS);										///< Set RS pin high to enter character mode.
+	LCD_PORT |= (1<<RS);							///< Set RS pin high to enter character mode.
 	LCD_PORT |= (1<<EN);
 	_delay_us(1);
-	LCD_PORT &= ~(1<<EN);										///< Create a momentary pulse at EN pin to send the data to the LCD.
+	LCD_PORT &= ~(1<<EN);							///< Create a momentary pulse at EN pin to send the data to the LCD.
 	
 	_delay_us(200);
 
 	LCD_PORT = (LCD_PORT & 0x0F)|(character << 4);				///< Load lower nibble of the character to the port. 
 	LCD_PORT |= (1<<EN);
 	_delay_us(1);
-	LCD_PORT &= ~(1<<EN);										///< Pulse to send the data.
+	LCD_PORT &= ~(1<<EN);							///< Pulse to send the data.
 	
 	_delay_ms(2);
 }
@@ -183,13 +183,13 @@ void LCD_SEND_STRING(char *string){
 	uint8_t	dig[3];
 	uint8_t integer = (uint8_t)number;
 	
-	dig[0] = (integer/100);								///< Save first digit to array.
-	dig[1] = (integer%100)/10;								///< Save second digit to array.
-	dig[2] = (integer%10);									///< Save third digit to array.
+	dig[0] = (integer/100);							///< Save first digit to array.
+	dig[1] = (integer%100)/10;						///< Save second digit to array.
+	dig[2] = (integer%10);							///< Save third digit to array.
 	
 	if(decimal){
 		for(uint8_t i=0;i<3;i++){
-			LCD_SEND_CHARACTER(((0x03<<4)|dig[i]));		///< Map digit to corresponding symbol in LCD character map.
+			LCD_SEND_CHARACTER(((0x03<<4)|dig[i]));			///< Map digit to corresponding symbol in LCD character map.
 			_delay_ms(2);
 		}
 	}
